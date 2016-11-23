cbd9e0464b900a5e743167f40619bd45f45ddb99
JiaLi.Passion <JiaLi.Passion@gmail.com>
2016-11-23 12:49:24 +0800

    change var to const

--- lib/node/node.ts
+++ lib/node/node.ts
@@ -32,7 +32,7 @@ if (shouldPatchGlobalTimers) {
 }
 
 // patch process.nextTick
-var nativeNextTick = process.nextTick;
+const nativeNextTick = process.nextTick;
 process.nextTick = function() {
   var args = arguments;
   args[0] = Zone.current.wrap(args[0], 'process.nextTick');
