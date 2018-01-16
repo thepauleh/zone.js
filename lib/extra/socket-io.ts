f3b58165d43a09aa58a3a1be1088c598ceb35d9b
JiaLi.Passion <JiaLi.Passion@gmail.com>
2018-01-16 22:39:57 +0900

    feat(patch): fix #828, patch socket.io client

---
+++ lib/extra/socket-io.ts
@@ -0,0 +1,21 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+Zone.__load_patch('socketio', (global: any, Zone: ZoneType, api: _ZonePrivate) => {
+  (Zone as any)[Zone.__symbol__('socketio')] = function patchEmitter(io: any) {
+    // io is being mixed with Emitter.prototype, so we can patchEventTargetMethods
+    // with io.prototype
+    api.patchEventTarget(global, [io.prototype], {
+      useG: false,
+      chkDup: false,
+      rt: true,
+      diff: (task: any, delegate: any) => {
+        return task.callback === delegate;
+      }
+    });
+  };
+});
